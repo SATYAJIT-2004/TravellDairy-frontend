a4ceb2ea7dfe9bef6861d0866366e23ff195cd04
SATYAJIT-2004 <satyajit2568@gmail.com>
2026-01-11 13:51:24 +0530

    Bakcend url corrected

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://travelldairy-backend.onrender.com/api"
+const BASE_URL = "https://travelldairy-backend.onrender.com"
 
 const axiosInstance = axios.create({
    baseURL: BASE_URL,
